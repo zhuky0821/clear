b299d9b0fd6c5b39be821153194cc4304fefceac
zhuky <2090313856@qq.com>
2020-11-24 13:34:06 +0800

    搭建ignite连接池

--- src/main/java/zhuky/clear/ClearApplication.java
+++ src/main/java/zhuky/clear/ClearApplication.java
@@ -1,28 +1,13 @@
 package zhuky.clear;
 
-import org.apache.ignite.IgniteCheckedException;
-import org.apache.ignite.IgniteLogger;
-import org.apache.ignite.logger.log4j2.Log4J2Logger;
-import org.apache.ignite.springframework.boot.autoconfigure.IgniteConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClearApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClearApplication.class, args);
-	}
-
-
-	@Bean
-	public IgniteConfigurer configurer() {
-		return cfg -> {
-			//Setting consistent id.
-			//See `application.yml` for the additional properties.
-			cfg.setConsistentId("clear");
-		};
 	}
 
 }
--- src/test/java/zhuky/clear/ClearApplicationTests.java
+++ src/test/java/zhuky/clear/ClearApplicationTests.java
@@ -1,13 +1,40 @@
 package zhuky.clear;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootTest
 class ClearApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	DataSource dataSource;
+	//private DataSource dataSource;
+	@Test
+	void testJdbc(){
+		try {
+			Connection conn = dataSource.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement("select * from tproduct");
+
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()){
+				System.out.println(resultSet.getInt("product_id") + " " + resultSet.getString("product_code"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
