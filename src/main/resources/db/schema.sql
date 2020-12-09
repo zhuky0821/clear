1e2360c5a88988bb0b15b405a9e6490f5ca053ee
zhukeyu <2090313856@qq.com>
2020-12-09 20:09:42 +0800

    jsmx文件生成

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -105,6 +105,19 @@ create table tfilecolumnconfig(
 );
 
 -- 业务表
+drop table if exists torder;
+create table torder(
+    product_id int,
+    unit_id int,
+    combi_id int,
+    report_serial_id int,
+    security_id int,
+    shareholdeer_id varchar (32),
+    invest_type int,
+    constraint torder_0 primary key(report_serial_id)
+);
+create index torder_1 on torder(product_id);
+
 
 drop table if exists  tcombistock;
 
